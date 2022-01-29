4b4166c074df3274585c8d6bc4f4fbdfb1c314cd
张佳群 <zhangjiaqun@37.com>
2022-01-29 16:00:23 +0800

    新增加克隆FBX

--- js/cloneFbx.js
+++ js/cloneFbx.js
@@ -4,31 +4,9 @@ import {TrackballControls} from "../examples/jsm/controls/TrackballControls.js";
 import {OrbitControls} from "../examples/jsm/controls/OrbitControls.js";
 import * as SkeletonUtils from "../examples/jsm/utils/SkeletonUtils.js";
 
-
-//将多有的骨骼节点存储到一个数组中
-function getBoneArray(obj, boneArr){
-    for(let i=0; i<obj.children.length; i++){
-        if (obj.children[i].isBone) {
-            boneArr[obj.children[i].name] = obj.children[i];
-        }
-        getBoneArray(obj.children[i], boneArr);
-    }
-}
-
-//将多有的骨骼节点存储到一个数组中
-function getSkeletonBoneArray(obj, boneArr){
-    console.log("obj.skeletons:");
-    console.log(obj.skeletons);
-    for(let i=0; i<obj.skeletons.length; i++){
-        boneArr[obj.skeletons[i].name] = obj.skeletons[i].bones;
-    }
-}
-
-
-
-let skinnedMesh;
 let scene = new THREE.Scene();
 let mixer=null;//声明一个混合器变量
+let newMixer=null;//声明一个混合器变量
 let loader = new FBXLoader();//创建一个FBX加载器
 loader.load("./fbx/c_with_skin.fbx", function(obj) {
     console.log("obj.json");
@@ -37,67 +15,51 @@ loader.load("./fbx/c_with_skin.fbx", function(obj) {
     // 骨骼辅助对象
     const skeletonHelper = new THREE.SkeletonHelper( obj );
     scene.add( skeletonHelper );
-    console.log("skeletonHelper:");
-    console.log(skeletonHelper);
     // obj.translateY(-80);
     // obj作为参数创建一个混合器，解析播放obj及其子对象包含的动画数据
     mixer = new THREE.AnimationMixer(obj);
-    console.log("mixer:");
-    console.log(mixer);
     // 查看动画数据
+    console.log("obj.animations:");
     console.log(obj.animations)
+    console.log("obj.mixer:");
+    console.log(mixer);
     // obj.animations[0]：获得剪辑对象clip
     let AnimationAction=mixer.clipAction(obj.animations[0]);
     // AnimationAction.timeScale = 1; //默认1，可以调节播放速度
     // AnimationAction.loop = THREE.LoopOnce; //不循环播放
     // AnimationAction.clampWhenFinished=true;//暂停在最后一帧播放的状态
+    console.log("AnimationAction:");
+    console.log(AnimationAction);
     AnimationAction.play();//播放动画
 
+    obj.traverse( function ( child ) {
+        if ( child.isMesh ) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    } );
+
+
     // 复制对象
-    let newObj = obj.clone(false);
-    obj.children.forEach(chd => {
-        console.log("child:");
-        console.log(chd.toJSON());
-        console.log("child.skeletons:");
-        console.log(chd.skeletons);
-    });
-    let newBone = obj.children[2].clone();
-    newObj.add(newBone);
-    console.log("newBone.toJSON():");
-    console.log(newBone.toJSON());
-
-    let cloneObj = SkeletonUtils.clone(obj);
-    console.log("cloneObj:");
-    console.log(cloneObj);
-
-    let boneArr = {};
-    getBoneArray(obj, boneArr);
-    console.log("boneArr:");
-    console.log(boneArr);
-    let skeletonBoneArr = {};
-    getSkeletonBoneArray(obj, skeletonBoneArr);
-    console.log("skeletonBoneArr:");
-    console.log(skeletonBoneArr);
-
-    //
-    // let skinMesh = obj.children[1].clone();
-    //
-    // console.log("skeleton.json:");
-    // console.log(obj.children[1].skeleton.toJSON());
-    // let newArr = [];
-    // obj.children[1].skeleton.bones.forEach(item=>{
-    //     newArr.push(boneArr[item.name]);
-    // });
-    //
-    // skinMesh.bind(new THREE.Skeleton(newArr, obj.children[1].boneInverses));
-    //
-    // newObj.add(skinMesh);
-    newObj.position.set(100, 0, 0);
-    newObj.scale.set(10,10,10);
+    let newObj = SkeletonUtils.clone(obj);
+    newObj.position.set(0, 200, 0);
+    newObj.scale.set(1, 1, 1);
+    newObj.animations = obj.animations;
+    newMixer = new THREE.AnimationMixer(newObj);
+    const newSkeletonHelper = new THREE.SkeletonHelper(newObj);
+    console.log("newObj.animations:");
+    console.log(newObj.animations);
+    console.log("newObj.mixer:");
+    console.log(newMixer);
+    let newAction = newMixer.clipAction(newObj.animations[0]);
+    // newAction.timeScale = AnimationAction.timeScale * AnimationAction.timeScale * 2 * 2 * AnimationAction.timeScale * 2 ; //默认1，可以调节播放速度
+    // newAction.loop = THREE.LoopRepeat; //不循环播放
+    // newAction.clampWhenFinished=true;//暂停在最后一帧播放的状态
+    console.log("newAction:");
+    console.log(newAction);
+    newAction.play();
     scene.add(newObj);
-    console.log("newObj.json");
-    console.log(newObj.toJSON());
-
+    scene.add( newSkeletonHelper );
 });
 
 //环境光:环境光颜色RGB成分分别和物体材质颜色RGB成分分别相乘
@@ -148,6 +110,11 @@ function render() {
         //clock.getDelta()方法获得两帧的时间间隔
         // 更新混合器相关的时间
         mixer.update(delta);
+    }
+    if (newMixer !== null) {
+        //clock.getDelta()方法获得两帧的时间间隔
+        // 更新混合器相关的时间
+        newMixer.update(delta);
     }
     if (controls !== null) {
         controls.update();
